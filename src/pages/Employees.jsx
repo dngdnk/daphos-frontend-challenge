0880ec44398b9feb4ffb344b847cd365c4acb44b
doan <Wutheringd@n1>
2025-11-16 01:59:43 +0100

    Minor fixes

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -8,7 +8,7 @@ function Employees() {
     <div className="employees">
       <h1> Employee Overview </h1>
       <p className="caption"> Details about all your employees in one place.</p>
-      <SearchBar placeholder="Search employees by ID, Name or Department" data={EmployeeData} />
+      <SearchBar placeholder="Search employees by ID, Name or" data={EmployeeData} />
     </div>
   );
 }
